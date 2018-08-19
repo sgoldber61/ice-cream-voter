cd2eb1ae6f43c67a27f674f22b265f79dbd5e25b
Mia <ngocmhuynh5@gmail.com>
2018-08-18 18:34:06 -0700

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,43 @@
 import React, {Component} from 'react';
+import axios from 'axios';
+import Candidate from './Candidate';
 
-class App extends Component {
+export default class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      votes: {
+        chocolate: 0,
+        vanilla: 0,
+        matcha: 0,
+        earlgrey: 0,
+        durian: 0,
+      }
+    }
+    this.addLike = this.addLike.bind(this);
   }
   
+  addLike(flavor) {
+
+  };
+
   render() {
-    return <div>Hello!</div>;
+    // if not initalized, do nothing
+    if (!this.state.votes) return null;
+
+    const flavorLikes = Object.entries(this.state.votes);
+    const icecreamToBeRendered = flavorLikes.map((flavorLikes, index) => (
+      <Candidate flavor={flavorLikes[0]} likes={flavorLikes[1]} key={index} clickEvent={this.addLike}/>
+    ));
+
+    return (
+      <div>
+        <h1>Which icecream is da best</h1>
+        <br/><br />
+        <div className="main">
+          {icecreamToBeRendered}
+        </div>
+      </div>
+    )
   }
 }
-
-export default App;
